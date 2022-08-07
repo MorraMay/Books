563c9021a237fe1585b17700c70113ffe2f3da54
Morra <104777030+MorraMay@users.noreply.github.com>
2022-08-07 17:57:46 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,197 @@
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  // coverflowEffect: {
+  //   rotate: 50,
+  //   stretch: 0,
+  //   depth: 100,
+  //   modifier: 1,
+  //   slideShadows : true,
+  // },
+  
+  coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 60,
+        modifier: 10,
+        slideShadows : false,
+        
+    },
+    
+  });
+  
+  
+var galleryTop = new Swiper('.swiper-container.testimonial', {
+  speed: 400,
+  spaceBetween: 50,
+  direction: 'vertical',
+  pagination: {
+    clickable: true,
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+  thumbs: {
+    swiper: galleryThumbs
+    }
+  });
+
+
+  
+ const open = document.getElementById('open');
+ const modal_container = document.getElementById('modal_container');
+ const close = document.getElementById('close');
+
+ open.addEventListener('click', () => {
+  modal_container.classList.add('show');
+ });
+
+close.addEventListener('click', () => {
+  modal_container.classList.remove('show');
+ });
+ 
+
+ const open_2 = document.getElementById('open_2');
+ const modal_container_2 = document.getElementById('modal_container_2');
+ const close_2 = document.getElementById('close_2');
+
+ open_2.addEventListener('click', () => {
+  modal_container_2.classList.add('show');
+ });
+
+close_2.addEventListener('click', () => {
+  modal_container_2.classList.remove('show');
+ });
+
+
+ const open_3 = document.getElementById('open_3');
+ const modal_container_3 = document.getElementById('modal_container_3');
+ const close_3 = document.getElementById('close_3');
+
+ open_3.addEventListener('click', () => {
+  modal_container_3.classList.add('show');
+ });
+
+close_3.addEventListener('click', () => {
+  modal_container_3.classList.remove('show');
+ });
+
+
+ const open_4 = document.getElementById('open_4');
+ const modal_container_4 = document.getElementById('modal_container_4');
+ const close_4 = document.getElementById('close_4');
+
+ open_4.addEventListener('click', () => {
+  modal_container_4.classList.add('show');
+ });
+
+close_4.addEventListener('click', () => {
+  modal_container_4.classList.remove('show');
+ });
+
+
+ const open_5 = document.getElementById('open_5');
+ const modal_container_5 = document.getElementById('modal_container_5');
+ const close_5 = document.getElementById('close_5');
+
+ open_5.addEventListener('click', () => {
+  modal_container_5.classList.add('show');
+ });
+
+close_5.addEventListener('click', () => {
+  modal_container_5.classList.remove('show');
+ });
+
+
+ const open_6 = document.getElementById('open_6');
+ const modal_container_6 = document.getElementById('modal_container_6');
+ const close_6 = document.getElementById('close_6');
+
+ open_6.addEventListener('click', () => {
+  modal_container_6.classList.add('show');
+ });
+
+close_6.addEventListener('click', () => {
+  modal_container_6.classList.remove('show');
+ });
+
+
+ const open_7 = document.getElementById('open_7');
+ const modal_container_7 = document.getElementById('modal_container_7');
+ const close_7 = document.getElementById('close_7');
+
+ open_7.addEventListener('click', () => {
+  modal_container_7.classList.add('show');
+ });
+
+close_7.addEventListener('click', () => {
+  modal_container_7.classList.remove('show');
+ });
+
+
+ 
+ const open_8 = document.getElementById('open_8');
+ const modal_container_8 = document.getElementById('modal_container_8');
+ const close_8 = document.getElementById('close_8');
+
+ open_8.addEventListener('click', () => {
+  modal_container_8.classList.add('show');
+ });
+
+close_8.addEventListener('click', () => {
+  modal_container_8.classList.remove('show');
+ });
+ 
+
+ 
+ const open_9 = document.getElementById('open_9');
+ const modal_container_9 = document.getElementById('modal_container_9');
+ const close_9 = document.getElementById('close_9');
+
+ open_9.addEventListener('click', () => {
+  modal_container_9.classList.add('show');
+ });
+
+close_9.addEventListener('click', () => {
+  modal_container_9.classList.remove('show');
+ });
+
+
+ const open_10 = document.getElementById('open_10');
+ const modal_container_10 = document.getElementById('modal_container_10');
+ const close_10 = document.getElementById('close_10');
+
+ open_10.addEventListener('click', () => {
+  modal_container_10.classList.add('show');
+ });
+
+close_10.addEventListener('click', () => {
+  modal_container_10.classList.remove('show');
+ });
+
+
+ const open_11 = document.getElementById('open_11');
+ const modal_container_11 = document.getElementById('modal_container_11');
+ const close_11 = document.getElementById('close_11');
+
+ open_11.addEventListener('click', () => {
+  modal_container_11.classList.add('show');
+ });
+
+close_11.addEventListener('click', () => {
+  modal_container_11.classList.remove('show');
+ });
+
+
+ const open_12 = document.getElementById('open_12');
+ const modal_container_12 = document.getElementById('modal_container_12');
+ const close_12 = document.getElementById('close_12');
+
+ open_12.addEventListener('click', () => {
+  modal_container_12.classList.add('show');
+ });
+
+close_12.addEventListener('click', () => {
+  modal_container_12.classList.remove('show');
+ });
